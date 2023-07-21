4a58a9a4c58d5164be627f1bab8ebf83b9a3837e
ThomasZRE <99052940+ThomasZRE@users.noreply.github.com>
2023-07-21 22:26:19 +0000

    Add grid

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const container = document.querySelector(".container");
 let div = document.createElement("div");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16**2; i++) {
     container.appendChild(document.createElement("div"));
 }
+
+const divArray = document.querySelectorAll("div");
+
+let arr = Array.from(divArray);
+
+arr.shift();
+
+arr.forEach(dv => dv.innerText = "\n\n");
+arr.forEach(dv => dv.addEventListener("mouseover", function(n) {
+    n.
+}));
