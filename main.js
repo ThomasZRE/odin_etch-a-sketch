47bc0db1cd32b509fae864aee24c4f361d341660
ThomasZRE <99052940+ThomasZRE@users.noreply.github.com>
2023-07-27 16:16:28 -0500

    Add almost finished grid

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+/* Button for input */
+const button = document.querySelector(".button");
+button.innerText = "Squares per side"
+
+// input button div child
+const input = document.createElement("input");
+const submit = document.createElement("button");
+
+// input data
+input.alt = "16";
+
+// submit data
+submit.type = "submit";
+submit.innerText = "Submit";
+submit.classList.add("submit");
+
+button.append(input, submit);
+
+
+
+/* Grid logic */
 const container = document.querySelector(".container");
 let div = document.createElement("div");
-
-let squares = parseInt(prompt("Squares per side: "));
+let squares = 16
+let info = input.value
+if (info) squares = input;
 
 if (squares > 30) {
     squares = 16
@@ -16,7 +38,12 @@ const divArray = document.querySelectorAll("div");
 let arr = Array.from(divArray);
 
 arr.shift();
+arr.shift();
 
-arr.forEach(dv => dv.innerText = "\n\n");
+arr.forEach(dv => {
+    let value = ((100 / squares) - ( (100 / squares) / 100 ) ) - 0.2;
+    document.documentElement.style.setProperty(`--valor`, `${value}%`);
+});
+
+arr.forEach(dv => dv.innerText = "\n");
 arr.forEach(dv => dv.addEventListener("mouseover", () => dv.style.background = "red"));
-
